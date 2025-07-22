928b762101b081fef5fc57911e46df70f47439b3
Burmsey <joehurndell1@gmail.com>
2025-07-22 13:39:37 +1200

    Both buttons are functional now woahhh

--- kivvy.py
+++ kivvy.py
@@ -1,8 +1,16 @@
 from kivy.app import App
 from kivy.lang import Builder
+import camDetect
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.filechooser import FileChooserListView
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
 
 KV = '''
 FloatLayout:
+    id: main_layout
 
     Image:
         source: 'Assets/secure security background.png'
@@ -30,22 +38,43 @@ FloatLayout:
                 text: "Start Camera"
                 font_size: 24
                 on_release:
-                    print("Start Camera button pressed!")
+                    app.init()
                 background_normal: ''
                 background_color: 0.2, 0.6, 1, 1
+
             Button:
                 text: "View Captures"
                 font_size: 24
                 on_release:
-                    print("View Captures button pressed!")
+                    app.open_file_explorer()
                 background_normal: ''
                 background_color: 0.2, 0.6, 1, 1
-
 '''
+
+class FileExplorer(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(orientation='vertical', **kwargs)
+        self.file_chooser = FileChooserListView(
+            path=r"C:\Users\joehu\Code Projects\School Projects\Secure-Security",  # Change this to your folder
+            filters=["*.mp4"],  #files types user can see
+        )
+
+        self.add_widget(self.file_chooser)
+
 
 class SimpleHomeApp(App):
     def build(self):
         return Builder.load_string(KV)
 
+    def init(self):
+        camDetect.init()
+
+    def open_file_explorer(self):
+        content = FileExplorer()
+        popup = Popup(title="Choose a file", content=content,
+                      size_hint=(0.9, 0.9))
+        popup.open()
+
 if __name__ == "__main__":
     SimpleHomeApp().run()
+
